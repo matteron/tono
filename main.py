a27eb9ba9b5428a3f831c003fef44b64b0c1400d
mattia.schiano@uconn.edu <mattia.schiano@uconn.edu>
2018-03-01 14:23:58 -0500

    Updated main.py to use multithreading for rfid.py and adapt for changes in btoh rfid.py and buttons.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from rfid import RFID
 from cartData import cartData
 from buttons import Buttons
+from threading import Thread
+
+# Global Variables for Threading with RFID scanner.
+global cartStatus		#True for a change, false for the same.
+cartStatus = False
+global readID			# -1 for no cart found.
+readID = "-1"
+global cartActive	
+cartActive = False 		#
 
 reader = RFID()
+readerThread = Thread(target=reader.run)
 cartData = cartData()
 b = Buttons(7,11,13,15)
 
@@ -10,33 +20,40 @@ regState = False
 playing = False
 playingID = "0"
 playingPath = "0"
-uid = "0"
 
-pwr = b.pwr() 
+pwr = b.pwr()
 fwd = b.fwd()
 pau = b.pau()
 bak = b.bak()
 
+readerThread.start()
+
 while True:
+	if pwr:
+		b.resetStates()
+		break
 	if playing:
-		uid = reader.getUID()
-		if uid != playingID:
-			#Stop Playing
-			pass
+		if cartStatus:
+			playing = False
+		if pau:
+			b.resetStates()
+			#pause track
+		if fwd:
+			b.resetStates()
+			#forward track
+		if bak:
+			b.resetStates()
+			#previous track
 	else:
-		if regState:
-			#display UID and then wait for stop button
-			pass
-		else:
-			if(pau):
-				print "hi"
-			uid = reader.getUID()
-			if uid != None:
-				playingID = uid
-				playingPath = cartData.getCartPath(uid)
+		if cartStatus:
+			if cartActive:
+				cartStatus = False
+				playingID = readID
+				playingPath = cartData.getCartPath(readID)
 				if playingPath == None:
-					# Display UID to register new card.
-					pass
-				elif playingPath != None:
-					# Start Player
-					pass
+					regState = True
+				else:
+					playing = True
+					#start player here
+readerThread.terminate()
+#star shutdown
