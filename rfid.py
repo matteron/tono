2d5d3b8eff58ff5eee3d666f20991d071ea50a50
mattia.schiano@uconn.edu <mattia.schiano@uconn.edu>
2018-03-01 13:07:05 -0500

    Altered rfid.py to prepare for multithreading.

--- rfid.py
+++ rfid.py
@@ -1,22 +1,36 @@
 from pirc522 import RFID
 
+global cartStatus
+global readID
+
 reader = RFID()
 class RFID:
+	def __init__(self):
+		self.sentMessage = False
+		self.running = True
+	
+	def terminate(self):
+		self.running = False
 
-	def getUID(self):
-		
-		reader.wait_for_tag()
-		(err, tag_type) = reader.request()
-		if not err:
-			(err, uid) = reader.anticoll()
-			if err:
-				reader.stop_crypto()	
-				return None
+	def sendMessage(self, uid):
+		cartStatus = True
+		self.sentMessage = True
+		readID = uid
+
+	def run(self):
+		while self.running:
+			if self.sentMessage:
+				if !cartStatus:
+					self.sentMessage = False
 			else:
-				reader.stop_crypto()
-				return uid
-		else:
-			reader.stop_crypto()
-			return None
-	def cleanup(self):
-		reader.cleanup()
+				reader.wait_for_tag()
+				(err, tag_type) = reader.request()
+				if not err:
+					(err, uid) = reader.anticoll()
+					reader.stop_crypto()
+					if readID != uid:	
+						sendMessage(uid)
+				else:
+					if readID != "-1":
+						sendMessage("-1")
+		reader.cleanup			
